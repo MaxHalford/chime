41c337f11bb6526018dd3e0549f2769c26d6d36a
Paulo S. Costa <pauloscosta5@gmail.com>
2020-11-19 20:21:02 -0800

    Add home mock fixture

--- test_chime.py
+++ test_chime.py
@@ -17,6 +17,13 @@ def reload_chime():
     importlib.reload(chime)
 
 
+@pytest.fixture(scope='function', autouse=True)
+def mock_pathlib_home(monkeypatch: _pytest.monkeypatch.MonkeyPatch):
+    with tempfile.TemporaryDirectory() as home_dir:
+        home_dir_path = pathlib.Path(home_dir)
+        monkeypatch.setattr(pathlib.Path, name='home', value=lambda: home_dir_path)
+
+
 def test_speed():
     tic = time.time()
     chime.success()
